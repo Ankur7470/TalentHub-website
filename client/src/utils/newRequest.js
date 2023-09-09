5992b02caa1859873d31098364d72cde71ab4ffa
Ankur Sinha <ankur.sinha2019@gmail.com>
2023-09-09 23:21:48 +0530

    first commit

---
+++ client/src/utils/newRequest.js
@@ -0,0 +1,9 @@
+import axios from "axios";
+
+const newRequest = axios.create({
+    baseURL: "http://localhost:5000/backend/", withCredentials: true,
+    // baseURL: "https://talent-hub-freelance-backend.onrender.com/backend/", withCredentials: true,
+
+});
+
+export default newRequest;
