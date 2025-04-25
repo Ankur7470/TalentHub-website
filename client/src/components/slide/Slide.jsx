04001c3cb4f7dac14cc516fbdbb36ac6703212b4
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-04-25 15:05:42 +0530

    Change Api Url

--- client/src/components/slide/Slide.jsx
+++ client/src/components/slide/Slide.jsx
@@ -1,14 +1,42 @@
+// import React from "react";
+// import "./Slide.scss";
+// import Slider from "infinite-react-carousel";
+
+// const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+//   return (
+//     <div className="slide">
+//       <div className="container">
+//         <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
+//           {children}
+//         </Slider>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default Slide;
 import React from "react";
 import "./Slide.scss";
-import Slider from "infinite-react-carousel";
+import useEmblaCarousel from "embla-carousel-react";
 
 const Slide = ({ children, slidesToShow, arrowsScroll }) => {
+  const [emblaRef] = useEmblaCarousel({ 
+    loop: true,
+    slidesToScroll: arrowsScroll || 1,
+    align: "start",
+    containScroll: "trimSnaps",
+    dragFree: false,
+    slidesToShow: slidesToShow || 1
+  });
+
   return (
     <div className="slide">
       <div className="container">
-        <Slider slidesToShow={slidesToShow} arrowsScroll={arrowsScroll}>
-          {children}
-        </Slider>
+        <div className="embla" ref={emblaRef}>
+          <div className="embla__container">
+            {children}
+          </div>
+        </div>
       </div>
     </div>
   );
--- client/src/utils/newRequest.js
+++ client/src/utils/newRequest.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const newRequest = axios.create({
-    baseURL: "http://localhost:5000/backend/", withCredentials: true,
+    // baseURL: "http://localhost:5000/backend/", withCredentials: true,
+    baseURL: "https://talent-hub-website-navy.vercel.app/backend", withCredentials: true
+  
     // baseURL: "https://talent-hub-freelance-backend.onrender.com/backend/", withCredentials: true,
 
 });
