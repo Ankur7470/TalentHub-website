4905a3940eb74b641aacfeece242f321d389ea70
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-26 02:25:20 +0530

    Removed Cookies based Auth

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -17,42 +17,41 @@ export const register = async (req, res, next) => {
     next(err);
   }
 };
+
 export const login = async (req, res, next) => {
   try {
     const user = await User.findOne({ username: req.body.username });
-
     if (!user) return next(createError(404, "User not found!"));
 
     const isCorrect = bcrypt.compareSync(req.body.password, user.password);
-    if (!isCorrect)
-      return next(createError(400, "Wrong password or username!"));
+    if (!isCorrect) return next(createError(400, "Wrong password or username!"));
 
     const token = jwt.sign(
-      {
-        id: user._id,
-        isSeller: user.isSeller,
-      },
-      process.env.JWT_KEY
+      { id: user._id, role: user.role },
+      process.env.JWT_KEY,
+      { expiresIn: "1d" }
     );
 
     const { password, ...info } = user._doc;
-    res
-      .cookie("accessToken", token, {
-        httpOnly: true,
-      })
-      .status(200)
-      .send(info);
+
+    res.status(200).json({ token, user: info });
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const getMe = async (req, res, next) => {
+  try {
+    const user = await User.findById(req.userId).select("-password");
+    if (!user) return next(createError(404, "User not found!"));
+    res.status(200).json(user);
   } catch (err) {
     next(err);
   }
 };
 
 export const logout = async (req, res) => {
-  res
-    .clearCookie("accessToken", {
-      sameSite: "none",
-      secure: true,
-    })
-    .status(200)
-    .send("User has been logged out.");
+  // Just a dummy route if you want to call /logout from frontend
+  res.status(200).send("User logged out");
 };
+
