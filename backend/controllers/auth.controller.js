058e1101fb48a21a785fd57c010204a8674283b2
Ankur Sinha <ankur.sinha2019@gmail.com>
2025-05-30 16:17:06 +0530

    Fix Bug

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -2,8 +2,6 @@ import User from "../models/user.model.js";
 import createError from "../utils/createError.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
-import crypto from "crypto";
-import sendEmail from "../utils/sendEmail.js";
 
 export const register = async (req, res, next) => {
   try {
